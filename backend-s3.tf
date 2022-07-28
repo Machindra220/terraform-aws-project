f9fed16a0545102ab36eef460f084c3622740b92
Machindra220 <machindra220@gmail.com>
2022-07-28 13:19:15 +0530

    added keypair
    keypair file created

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 terraform {
   backend "s3" {
     bucket = "terraform-learn-bucket"
     key    = "terraform-vprofile-state/backend"
     region = "us-east-1"
   }
+=======
+terraform {
+  backend "s3" {
+    bucket = "terraform-learn-bucket"
+    key    = "terraform-vprofile-state/backend"
+    region = "us-east-1"
+  }
+>>>>>>> 66c7c51efff555a6a3285c7ad99beb5630ccb441
 }
